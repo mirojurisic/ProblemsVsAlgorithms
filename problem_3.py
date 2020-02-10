687c31ca696befc8e61700612b4cfed064a231be
mirojurisic <mirojurisic@hotmail.com>
2020-02-10 21:38:50 +0100

    problem 3 done

---
+++ problem_3.py
@@ -0,0 +1,51 @@
+"""
+Rearrange Array Elements
+Rearrange Array Elements so as to form two number such that their sum is maximum. Return these two numbers.
+You can assume that all array elements are in the range [0, 9].
+
+The number of digits in both the numbers cannot differ by more than 1.
+You're not allowed to use any sorting function that Python provides and the expected time complexity is O(nlog(n)).
+
+for e.g. [1, 2, 3, 4, 5]
+
+The expected answer would be [531, 42]. Another expected answer can be [542, 31].
+In scenarios such as these when there are more than one possible answers, return any one.
+
+Here is some boilerplate code and test cases to start with:
+"""
+
+
+def rearrange_digits(input_list):
+    """
+    Rearrange Array Elements so as to form two number such that their sum is maximum.
+
+    Args:
+       input_list(list): Input List
+    Returns:
+       (int),(int): Two maximum sums
+    """
+    first = ""
+    second = ""
+    one = 0
+    two = 0
+    input_list.sort(reverse=True)
+    for i in range(0, len(input_list), 2):
+        first+=str(input_list[i])
+        if i+1 < len(input_list):
+            second+=str(input_list[i+1])
+    first = int(first)
+    second = int(second)
+    return [first,second]
+
+
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
